ac4870dd57cdde918fb85cefa4b41d04455c6913
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-12-16 15:36:56 -0300

    :wrench: Adicionando conteúdo 003 de JS

---
+++ Documentação/JavaScript/01 - Nível Básico/003 - Alert/003 - script.js
@@ -0,0 +1,15 @@
+// Sabe aquelas caixinhas de diálogo que aparecem para nós as vezes nos sites? É o que iremos aprender agora.
+
+alert("Hello World!"); // Aparece uma caixa de diálogo com uma opção de ok.
+
+confirm("Você deseja acessar o site?"); // Aparece uma caixa de diálogo com a opção de ok ou cancelar.
+
+prompt("Qual a sua idade?"); // Aparece uma caixa de diálogo com uma caixa para você digitar algo e uma opção de ok.
+
+// Da mesma forma com o console.log, também podemos colocar variáveis dentro das mensagens de alert, prompt e confirm.
+
+var nome = "Wesley";
+
+alert(nome);
+
+// Lembre-se: é muito importante continuar entrando no console do seu navegador para testar isso.
