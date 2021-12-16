a6a99dc248601dbe9e4a9bc3d4308e83f8e546bb
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-12-16 15:21:21 -0300

    :wrench Adicionando conteúdo 001 de JS

---
+++ Documentação/JavaScript/01 - Nível Básico/001 - Variáveis/001 - script.js
@@ -0,0 +1,12 @@
+/* 
+Vamos falar um pouco sobre variáveis em JavaScript. 
+Variável como o nome já diz, é algo que pode ser mutavel e atribuido, em JavaScript utilizamos variáveis para atribuir valores que queremos de alguma forma utilizarmos mais a frente.
+*/
+
+// Existem três tipos de variáveis
+
+var nome = "Wesley"; // A variável do tipo string, que nada mais é do que um texto delimitado pelas aspas duplas("")
+
+var idade = 21; // A variável do tipo number, que são números.
+
+var maiorDeIdade = true; // A variável do tipo bolean, que recebe os atributos "true" e "false".
