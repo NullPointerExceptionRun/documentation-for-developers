546d1e300ca8f644a17436f68792efb860be4587
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-12-27 18:06:41 -0300

    :wrench: Alterando conteúdo 002 de JS

--- Documentação/JavaScript/01 - Nível Básico/002 - Console.log/002 - script.js
+++ Documentação/JavaScript/01 - Nível Básico/002 - Console.log/002 - script.js
@@ -1,5 +1,6 @@
 // O console.log é um comando que permite a nós desenvolvedores, consultar o valor de alguma coisa (variável, função e etc (que será melhor apresentado adiante)), como também atribuir mensagens para serem lidas pelo navegador.
 
+
 console.log("Hello World!"); // Dentro do console do navegador será impresso a mensagem "Hello World!".
 
 var nome = "Wesley"; // Variável do tipo string.
