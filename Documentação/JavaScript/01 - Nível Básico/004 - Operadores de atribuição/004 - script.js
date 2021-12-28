0ef232755d85b99928a48afaa0702f20593d4477
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2021-12-28 20:36:55 -0300

    :wrench: Adicionando conteúdo 004 de JS

---
+++ Documentação/JavaScript/01 - Nível Básico/004 - Operadores de atribuição/004 - script.js
@@ -0,0 +1,32 @@
+// No conteúdo 004 iremos começar a tratar com operadores, no JavaScript os operadores são muito importante para manipularmos nossos scripts, aqui começaremos falando dos operadores de atribuição.
+
+// Operadores de Atribuição:
+
+// 1 - Atribuição:
+
+var x = 10 // O que fizemos básicamente foi atribuir o valor 10 a variável x, então quando tentarmos imprimir a variável x, sairá um 10.
+
+// 2 - Adição:
+
+var x = 10
+x += 5 // Utilizando esse operador, podemos somar valores a nossa variável que a princício valia 10, porém quando colocamos que x += 5, a variável agora passa a valer 15, é como se tivesse dito que "var x = 10 + 5", porém dessa forma que fizemos, podemos alterar o valor da variável com base no seu antigo valor.
+
+// 3 - Subtração:
+
+var x = 15
+x -= 5 // Assim como no caso da adição, onde somamos o valor 5 a variável x, na subtração agora diminuimos 5 do seu valor, ou seja, x agora passa a valer 10.
+
+// 4 - Multiplicação:
+
+var x = 10
+x *= 2 // Utilizamos esse operador para multiplicarmos o valor da variável por 2, a partir disso, x passa a valer 20.
+
+// 5 - Divisão:
+
+var x = 50
+x /= 2 // Utilizamos esse operador para dividirmos o valor da variável por 2, a partir disso, x passa a valer 25.
+
+// 6 - Resto:
+
+var x = 20
+x %= 3 // Esse operador basicamente divide a variável pelo valor dado e atribui a variável o valor do resto dessa divisão, por exemplo 20/3 = 6 e resta 2, pois 3*6 = 18, então quando atribuimos isso, a variável x passa a valer 2, que é o resto da divisão de 20 por 3.
